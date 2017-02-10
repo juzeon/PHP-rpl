a48b9d20d8890d8c046d2a9f495570409c777fb2
juzeon <812312770@qq.com>
2017-02-10 12:26:46 +0800

    fix bugs,add the logger

--- rpl.php
+++ rpl.php
@@ -2,6 +2,8 @@
 $search='';
 $replace='';
 $basedir='.';
+$log=FALSE;
+$cs=1;
 if(isset($_GET['search'])&&isset($_GET['replace'])){
 	$search=$_GET['search'];
 	$replace=$_GET['replace'];
@@ -14,29 +16,42 @@ if(isset($_GET['search'])&&isset($_GET['replace'])){
 	if(empty($search)||empty($search)){
 		echo 'Cannot get required string.<br/>';
 	}else{
-		checkdir($basedir,$search,$replace);
+		checkdir($basedir,$search,$replace,$log);
 		echo 'Done!<br/><a href="?">Return</a><br/>';
 		exit(0);
 	}
 }
-function checkdir($basedir,$search,$replace) {
+function checkdir($basedir,$search,$replace,$log) {
+	$logfile=FALSE;
+	if(!empty($log)){
+		$logfile=fopen($log,'w');
+	}
 	if ($dh = opendir($basedir)) {
 		while (($file = readdir($dh)) !== false) {
 			if ($file != '.' && $file != '..') {
 				if (!is_dir($basedir . '/' . $file)) {
 					$result=file_modify($search,$replace, "$basedir/$file");
 					if($result){
-						echo 'Replaced file: ' . "$basedir/$file" . '<br/>';
+						echo time() . ': Replaced file: ' . "$basedir/$file" . '<br/>';
+						if(!empty($logfile)){
+							fwrite($logfile,time() . ': Replaced file: ' . "$basedir/$file" . PHP_EOL);
+						}
 					}else{
-						echo 'Skipped file: ' . "$basedir/$file" . '<br/>';
+						echo time() . ': Skipped file: ' . "$basedir/$file" . '<br/>';
+						if(!empty($logfile)){
+							fwrite($logfile,time() . ': Skipped file: ' . "$basedir/$file" . PHP_EOL);
+						}
 					}
 				} else {
 					$dirname = $basedir . '/' . $file;
-					checkdir($dirname);
+					checkdir($dirname,$search,$replace,$log);
 				}
 			}
 		}
 		closedir($dh);
+	}
+	if(!empty($logfile)){
+		fclose($logfile);
 	}
 }
 function file_modify($search_contents, $replace_contents, $filename) {
@@ -56,11 +71,18 @@ function file_modify($search_contents, $replace_contents, $filename) {
 	</head>
 	<body>
 		<h1>PHP-rpl</h1>
+		Replace all strings in all files easily. <br />
+		<hr />
+		<h2>Settings:</h2>
 		<form action="" method="get">
-			Base dir:<input type="text" name="basedir" id="basedir" value="<?php echo $basedir; ?>" />($_GET['basedir']) <br />
-			Search string:<input type="text" name="search" id="search" value="<?php echo $search; ?>" />($_GET['search']) <br />
-			Replace string:<input type="text" name="replace" id="replace" value="<?php echo $replace; ?>" />($_GET['replace']) <br />
-			<input type="submit" value="Go"/><br /><br />
+			Base dir: <input type="text" name="basedir" id="basedir" value="<?php echo $basedir; ?>" />($_GET['basedir']) <br />
+			Search string: <input type="text" name="search" id="search" value="<?php echo $search; ?>" />($_GET['search']) <br />
+			Replace string: <input type="text" name="replace" id="replace" value="<?php echo $replace; ?>" />($_GET['replace']) <br />
+			<input type="submit" value="Go"/><br />
+			<hr />
+			<h2>Advanced:</h2>
+			Case sensitive：<input type="checkbox" checked="true" name="cs" value="<?php echo $cs; ?>" />($_GET['cs'])<br />
+			Log to file: <input type="text" name="log" id="log" value="<?php echo $log; ?>" />($_GET['log']) example:<?php echo time().'.log'; ?><br />
 		</form>
 		by Github <a href="https://github.com/juzeon">@juzeon</a><br />
 	</body>
