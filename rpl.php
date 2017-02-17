142e7e1f175fffd4a5c2d3f8a580e42a1268f106
juzeon <812312770@qq.com>
2017-02-17 20:03:50 +0800

    fix logger,add case sensitive

--- rpl.php
+++ rpl.php
@@ -3,10 +3,33 @@ $search='';
 $replace='';
 $basedir='.';
 $log=FALSE;
+$GLOBALS['logger']='';
 $cs=1;
 if(isset($_GET['search'])&&isset($_GET['replace'])){
 	$search=$_GET['search'];
 	$replace=$_GET['replace'];
+	if(isset($_GET['log'])){
+		$log=$_GET['log'];
+		if(!empty($log)){
+			echo 'Logger is enabled<br/>';
+			$GLOBALS['logger'].='Logger is enabled' . PHP_EOL;
+		}else{
+			echo 'Logger is disabled<br/>';
+		}
+	}
+	if(isset($_GET['cs'])){
+		$cs=TRUE;
+		echo 'Case sensitive is enabled<br/>';
+		if(!empty($log)){
+			$GLOBALS['logger'].='Case sensitive is enabled' . PHP_EOL;
+		}
+	}else{
+		$cs=FALSE;
+		echo 'Case sensitive is disabled<br/>';
+		if(!empty($log)){
+			$GLOBALS['logger'].='Case sensitive is disabled' . PHP_EOL;
+		}
+	}
 	if(isset($_GET['basedir'])){
 		$basedir=$_GET['basedir'];
 		if($basedir[strlen($basedir)-1]=='/'){
@@ -16,50 +39,56 @@ if(isset($_GET['search'])&&isset($_GET['replace'])){
 	if(empty($search)||empty($search)){
 		echo 'Cannot get required string.<br/>';
 	}else{
-		checkdir($basedir,$search,$replace,$log);
+		checkdir($basedir,$search,$replace,$log,$cs);
 		echo 'Done!<br/><a href="?">Return</a><br/>';
+		$GLOBALS['logger'].='Done!'.PHP_EOL;
+		if(!empty($log)){
+			file_put_contents($log,$GLOBALS['logger']);
+		}
 		exit(0);
 	}
 }
-function checkdir($basedir,$search,$replace,$log) {
-	$logfile=FALSE;
-	if(!empty($log)){
-		$logfile=fopen($log,'w');
-	}
+function checkdir($basedir,$search,$replace,$log,$cs) {
 	if ($dh = opendir($basedir)) {
 		while (($file = readdir($dh)) !== false) {
 			if ($file != '.' && $file != '..') {
 				if (!is_dir($basedir . '/' . $file)) {
-					$result=file_modify($search,$replace, "$basedir/$file");
+					if($file!=$log){
+						$result=file_modify($search,$replace, "$basedir/$file",$cs);
+					}
 					if($result){
-						echo time() . ': Replaced file: ' . "$basedir/$file" . '<br/>';
-						if(!empty($logfile)){
-							fwrite($logfile,time() . ': Replaced file: ' . "$basedir/$file" . PHP_EOL);
+						echo '<font color="red">'.time() . ': Replaced file: ' . "$basedir/$file" . '</font><br/>';
+						if(!empty($log)){
+							$GLOBALS['logger'].=time() . ': Replaced file: ' . "$basedir/$file" . PHP_EOL;
 						}
 					}else{
 						echo time() . ': Skipped file: ' . "$basedir/$file" . '<br/>';
-						if(!empty($logfile)){
-							fwrite($logfile,time() . ': Skipped file: ' . "$basedir/$file" . PHP_EOL);
+						if(!empty($log)){
+							$GLOBALS['logger'].=time() . ': Skipped file: ' . "$basedir/$file" . PHP_EOL;
 						}
 					}
 				} else {
 					$dirname = $basedir . '/' . $file;
-					checkdir($dirname,$search,$replace,$log);
+					checkdir($dirname,$search,$replace,$log,$cs);
 				}
 			}
 		}
 		closedir($dh);
 	}
-	if(!empty($logfile)){
-		fclose($logfile);
-	}
 }
-function file_modify($search_contents, $replace_contents, $filename) {
+function file_modify($search_contents, $replace_contents, $filename,$cs) {
 	$fp = file_get_contents($filename);
-	if(strpos($fp,$search_contents)===FALSE){
-		return FALSE;
+	if($cs==TRUE){
+		if(strpos($fp,$search_contents)===FALSE){
+			return FALSE;
+		}
+		$new_fp = str_replace($search_contents, $replace_contents, $fp);
+	}else{
+		if(stripos($fp,$search_contents)===FALSE){
+			return FALSE;
+		}
+		$new_fp = str_ireplace($search_contents, $replace_contents, $fp);
 	}
-	$new_fp = str_replace($search_contents, $replace_contents, $fp);
 	file_put_contents($filename, $new_fp);
 	return TRUE;
 }
